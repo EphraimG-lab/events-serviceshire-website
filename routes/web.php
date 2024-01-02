1e0237a0c6a48e8743b81de46c8c8335ac5fbbac
Gabu-Rayon <gibsonmurimi4@gmail.com>
2024-01-02 16:55:16 +0300

    Commits

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\BleedingRhymesController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes for Bleeding Rhymes Creatives
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
@@ -16,28 +16,28 @@ use App\Http\Controllers\BleedingRhymesController;
 */
 
 //BleedingRhymesMain Website Routes
-Route::get('/',[BleedingRhymesController::class,'index']);
-Route::get('/about',[BleedingRhymesController::class,'about']);
+Route::get('/', [BleedingRhymesController::class,'index']);
+Route::get('/about', [BleedingRhymesController::class,'about']);
 
-Route::get('/blog',[BleedingRhymesController::class,'blog']);
-Route::get('/contact',[BleedingRhymesController::class,'contact']);
+Route::get('/blog', [BleedingRhymesController::class,'blog']);
+Route::get('/contact', [BleedingRhymesController::class,'contact']);
 
-Route::get('/cookies',[BleedingRhymesController::class,'cookies']);
-Route::get('/events',[BleedingRhymesController::class,'events']);
+Route::get('/cookies', [BleedingRhymesController::class,'cookies']);
+Route::get('/events', [BleedingRhymesController::class,'events']);
 
-Route::get('/FAQs',[BleedingRhymesController::class,'FAQs']);
-Route::get('/privacy',[BleedingRhymesController::class,'privacy']);
+Route::get('/FAQs', [BleedingRhymesController::class,'FAQs']);
+Route::get('/privacy', [BleedingRhymesController::class,'privacy']);
 
-Route::get('/services',[BleedingRhymesController::class,'services']);
-Route::get('/speakers',[BleedingRhymesController::class,'speakers']);
+Route::get('/services', [BleedingRhymesController::class,'services']);
+Route::get('/speakers', [BleedingRhymesController::class,'speakers']);
 
 
 //BleedingRhymesUser Routes controllers
-Route::get('/sign-up',[BleedingUserController::class,'register']);
-Route::post('/sign-up',[BleedingUserController::class,'create']);
+Route::get('/sign-up', [BleedingUserController::class,'register']);
+Route::post('/sign-up', [BleedingUserController::class,'create']);
 
-Route::get('/sign-in',[BleedingUserController::class,'login']);
-Route::post('/sign-in',[BleedingUserController::class,'login']);
+Route::get('/sign-in', [BleedingUserController::class,'login']);
+Route::post('/sign-in', [BleedingUserController::class,'login']);
 
-Route::get('/cart',[BleedingUserController::class,'cart']);
-Route::get('/buy-tickets',[BleedingUserController::class,'buy']);
+Route::get('/cart', [BleedingUserController::class,'cart']);
+Route::get('/buy-tickets', [BleedingUserController::class,'buy']);
